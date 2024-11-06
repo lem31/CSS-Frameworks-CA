b6e835deeddea098b227f75e3ceda3aa38dfdb62
Leanne <lemeyer.no@gmail.com>
2024-11-06 13:45:59 +0100

    Updated tailwind config content so that tailwind also works on auth files and added tailwind styles to auth html for logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./auth/**/*.{html,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
