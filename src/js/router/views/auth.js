a00f498ae007e9657f1f9341e2d0cf960ed61307
Leanne <lemeyer.no@gmail.com>
2024-11-06 13:18:38 +0100

    Added code to auth.js so that login form is displayed when clicking on the login button

--- src/js/router/views/auth.js
+++ src/js/router/views/auth.js
@@ -6,6 +6,10 @@ import { onLogin } from "../../ui/auth/login";
 
 import { onRegister } from "../../ui/auth/register";
 
+const LOGIN_FORM = document.getElementById("login-form");
+
+const LOGIN_BUTTON = document.getElementById("login-button");
+
 /**
  * Listens for the login form submission event and calls the onLogin function.
  * @function listenForLoginFormSubmission
@@ -14,7 +18,6 @@ import { onRegister } from "../../ui/auth/register";
  * listenForLoginFormSubmission();
  */
 function listenForLoginFormSubmission() {
-  const LOGIN_FORM = document.getElementById("login-form");
   LOGIN_FORM.addEventListener("submit", onLogin);
 }
 
@@ -23,3 +26,13 @@ listenForLoginFormSubmission();
 //REGISTER FORM CODE
 
 onRegister();
+
+LOGIN_BUTTON.addEventListener("click", showLoginForm);
+
+function showLoginForm() {
+  if (LOGIN_FORM.style.display === "none") {
+    LOGIN_FORM.style.display = "block";
+  } else {
+    LOGIN_FORM.style.display = "none";
+  }
+}
