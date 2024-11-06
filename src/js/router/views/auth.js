97ee3fe72520fa6808a7b059f12d1e9d9c0acb98
Leanne <lemeyer.no@gmail.com>
2024-11-06 10:49:46 +0100

    Added hidden class to login and register forms in auth html file and then added js code for both forms in auth.js

--- src/js/router/views/auth.js
+++ src/js/router/views/auth.js
@@ -3,3 +3,27 @@ import { authGuard } from "../../utilities/authGuard";
 alert("Auth Index Page");
 
 authGuard();
+
+//LOGIN FORM CODE
+
+import { onLogin } from "../../ui/auth/login";
+import { LOGIN_FORM } from "../../api/constants.js";
+
+/**
+ * Listens for the login form submission event and calls the onLogin function.
+ * @function listenForLoginFormSubmission
+ * @returns {void}
+ * @example
+ * listenForLoginFormSubmission();
+ */
+function listenForLoginFormSubmission() {
+  LOGIN_FORM.addEventListener("submit", onLogin);
+}
+
+listenForLoginFormSubmission();
+
+//REGISTER FORM CODE
+
+import { onRegister } from "../../ui/auth/register";
+
+onRegister();
