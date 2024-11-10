9bc6c98422c5c81f692ca3bd5bb85bd3efb08644
Leanne <lemeyer.no@gmail.com>
2024-11-10 08:39:29 +0100

    changed deploy script to gh-pages

---
+++ dist/assets/authGuard-DesCLFY3.js
@@ -0,0 +1,1 @@
+function o(){localStorage.accessToken||(alert("You must be logged in to view this page"),window.location.href="/")}export{o as a};
---
+++ dist/assets/postCreate-C4VEK_F3.js
@@ -0,0 +1,1 @@
+import{o}from"./create--F_RGuHq.js";import{a as r}from"./authGuard-DesCLFY3.js";import"./headers-Dul6VsrZ.js";r();o();
