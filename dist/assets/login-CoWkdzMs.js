4baae413ce16ab44264067a829d2f6824a87ace3
Leanne <lemeyer.no@gmail.com>
2024-11-10 14:55:48 +0100

    Fix: fixed issue with create form

---
+++ dist/assets/login-CoWkdzMs.js
@@ -0,0 +1,1 @@
+import{o}from"./login-4Be7PTDZ.js";import"./headers-Dul6VsrZ.js";function n(){document.getElementById("login-form").addEventListener("submit",o)}n();
--- dist/assets/login-DQUAQ9ft.js
+++
@@ -1,1 +0,0 @@
-import{o}from"./login-CbkO7zmt.js";import"./headers-Dul6VsrZ.js";function n(){document.getElementById("login-form").addEventListener("submit",o)}n();
