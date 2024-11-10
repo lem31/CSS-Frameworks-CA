6888977d610c780fc4234dd11b0b439ea6605ade
Leanne <lemeyer.no@gmail.com>
2024-11-10 15:14:15 +0100

    Added max width to post titles on desktop and mobile

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -68,6 +68,8 @@ export class CreateAllPostElements {
     POST_TITLE.classList.add("dark:text-white");
     VIEW_POST_BUTTON.classList.add("dark:dark-mode-style");
     POST_BODY.classList.add("post-text-alignment");
+    POST_BODY.classList.add("post-body-width-mobile");
+    POST_BODY.classList.add("post-body-width-desktop");
 
     VIEW_POST_BUTTON.textContent = "View post";
     POST_TITLE.textContent = post.title || "No title available";
